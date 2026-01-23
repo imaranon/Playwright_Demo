39b71af38df9bd193eb0150ea5d54a8ba7400772
imaranon <ij.maranon@gmail.com>
2026-01-23 19:09:59 +0800

    adding dummy to see the folder

--- src/tests/example.spec.ts
+++ src/tests/example.spec.ts
@@ -8,12 +8,15 @@ test('has title', async ({ page }) => {
   await expect(page).toHaveTitle(/Practice Software Testing - Toolshop - v5.0/);
 });
 
-// test('get started link', async ({ page }) => {
-//   await page.goto('https://playwright.dev/');
+test('get started link', async ({ page }) => {
+  await page.goto('https://practicesoftwaretesting.com/');
 
-//   // Click the get started link.
-//   await page.getByRole('link', { name: 'Get started' }).click();
+  // Click the get started link.
+  await page.getByRole('link', { name: 'nav-sign-in' }).click();
 
-//   // Expects page to have a heading with the name of Installation.
-//   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
-// });
+  // Expects page to have a heading with the name of Installation.
+  // await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+
+  // Check for the page URL to be 'https://playwright.dev/docs/intro' (including query string)
+await expect(page).toHaveURL('https://practicesoftwaretesting.com/auth/login/');
+});
