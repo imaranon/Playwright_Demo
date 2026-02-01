90d23072c7a6813efabf70897796a75072c4b3ea
imaranon <ij.maranon@gmail.com>
2026-02-01 12:17:10 +0800

    commit example.spec

--- src/tests/example.spec.ts
+++ src/tests/example.spec.ts
@@ -1,29 +1,67 @@
 import { test, expect } from '@playwright/test';
+import { csvToJson } from '../utils/CsvtoJson';
+
 // import { baseURL } from '../playwright.config.ts'; 
 
-test.skip('has title', async ({ page }) => {
+// After each test
+// Logout
+// Clear cookies
+// Take screenshots on failure
+// Reset test data
+test.afterEach(async () => {
+  console.log ('Finished test of After each test...');
+  console.log('Test completed');
+});
+
+// After all tests
+// Delete test data
+// Close database connection
+// Stop server
+// Cleanup
+// Generate reports (custom)
+test.afterAll(async () => {
+  console.log('Finished test suite of After all tests...');
+  console.log('All Test completed');
+});
+// Before all tests
+// Create test data
+// Global setup
+// Start a server
+// Load test configuration
+test.beforeAll(async () => {
+  console.log('Starting test of Before all tests...');
+});
+
+// Before each test
+// Navigate to base URL
+// Login before every test
+// Reset app state
+test.beforeEach(async ({page}) => {
+  console.log ('Starting test of Before each test...');
   await page.goto('https://practicesoftwaretesting.com/');
+});
+
+
+
+
+//===============================================================================
+
+// Basic test to check the title of the page .toHaveTitle
+test('Homepage has title', async ({ page }) => {  
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Practice Software Testing - Toolshop - v5.0/);
 });
 
-test.skip('get started link', async ({ page }) => {
+// Test to check the get started link
+test('get started link', async ({ page }) => {
   await page.goto('https://practicesoftwaretesting.com/');
 
   // Click the get started link.
-  await page.getByRole('link', { name: 'nav-sign-in' }).click();
+  await page.getByRole('link', { name: 'Sign in' }).click();
+  //await page.getByTestId('sign-in-button').click();
 
-  // Expects page to have a heading with the name of Installation.
-  // await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
-
-  // Check for the page URL to be 'https://playwright.dev/docs/intro' (including query string)
-await expect(page).toHaveURL('https://practicesoftwaretesting.com/auth/login/');
-});
-
-test("Example dotenv test", async ({ page }) => {
-
-console.log(process.env.userID);
-console.log(process.env.userPassword);
-
+  await page.getByRole('textbox', { name: 'Email address' }).fill('admin@practicesoftwaretesting.com');
+  await page.getByRole('textbox', { name: 'Password' }).fill('welcome01');
+  await page.getByRole('button',{name: 'login'}).click()
 });
